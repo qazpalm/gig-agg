2ca1212f7770ab75f6857905124279ea9a96ae13
Bill Crooks <bcrooks234@gmail.com>
2025-04-02 19:00:49 +1100

    Add GigFilters struct to support filtering gigs by venue, artist, genre, and date range

--- internal/web/gig_filters.go
+++ internal/web/gig_filters.go
@@ -1,1 +1,14 @@
 package web
+
+import (
+	"time"
+)
+
+type GigFilters struct {
+	VenueID   *int64
+	ArtistID  *int64
+	GenreID   *int64
+	FromDate  *time.Time
+	ToDate    *time.Time
+	Query	  string
+}
