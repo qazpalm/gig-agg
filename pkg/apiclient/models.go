762df57082ed042eb74ff04eda152dadfe788fcf
Bill Crooks <bcrooks234@gmail.com>
2025-04-10 11:00:50 +1000

    Implement CRUD operations for genres, gigs, and venues in API client

--- pkg/apiclient/models.go
+++ pkg/apiclient/models.go
@@ -73,13 +73,140 @@ func (c *Client) GetArtist(id int) (Artist, error) {
 	return artist, nil
 }
 
-func (c *Client) UpdateArtist(artist Artist) (Artist, error) {
-	// Implement the API call to update an artist
-	return Artist{}, nil
+func (c *Client) UpdateArtist(artist Artist) error {
+	// Make the API call to update the artist
+	err := c.doRequest("PUT", fmt.Sprintf("artist/%d", artist.ID), artist, &artist)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func (c *Client) DeleteArtist(id int) error {
-	// Implement the API call to delete an artist
+	// Make the API call to delete the artist
+	err := c.doRequest("DELETE", fmt.Sprintf("artist/%d", id), nil, nil)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (c *Client) CreateGenre(genre Genre) error {
+	// Make the API call to create the genre
+	err := c.doRequest("POST", "genre", genre, nil)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (c *Client) GetGenre(id int) (Genre, error) {
+	genre := Genre{}
+	err := c.doRequest("GET", fmt.Sprintf("genre/%d", id), nil, &genre)
+	if err != nil {
+		return Genre{}, err
+	}
+	return genre, nil
+}
+
+func (c *Client) UpdateGenre(genre Genre) error {
+	// Make the API call to update the genre
+	err := c.doRequest("PUT", fmt.Sprintf("genre/%d", genre.ID), genre, &genre)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (c *Client) DeleteGenre(id int) error {
+	// Make the API call to delete the genre
+	err := c.doRequest("DELETE", fmt.Sprintf("genre/%d", id), nil, nil)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (c *Client) CreateGig(gig Gig) error {
+	// Make the API call to create the gig
+	err := c.doRequest("POST", "gig", gig, nil)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (c *Client) GetGig(id int) (Gig, error) {
+	gig := Gig{}
+	err := c.doRequest("GET", fmt.Sprintf("gig/%d", id), nil, &gig)
+	if err != nil {
+		return Gig{}, err
+	}
+	return gig, nil
+}
+
+func (c *Client) UpdateGig(gig Gig) error {
+	// Make the API call to update the gig
+	err := c.doRequest("PUT", fmt.Sprintf("gig/%d", gig.ID), gig, &gig)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (c *Client) DeleteGig(id int) error {
+	// Make the API call to delete the gig
+	err := c.doRequest("DELETE", fmt.Sprintf("gig/%d", id), nil, nil)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (c *Client) CreateVenue(venue Venue) error {
+	// Make the API call to create the venue
+	err := c.doRequest("POST", "venue", venue, nil)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (c *Client) GetVenue(id int) (Venue, error) {
+	venue := Venue{}
+	err := c.doRequest("GET", fmt.Sprintf("venue/%d", id), nil, &venue)
+	if err != nil {
+		return Venue{}, err
+	}
+	return venue, nil
+}
+
+func (c *Client) UpdateVenue(venue Venue) error {
+	// Make the API call to update the venue
+	err := c.doRequest("PUT", fmt.Sprintf("venue/%d", venue.ID), venue, &venue)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (c *Client) DeleteVenue(id int) error {
+	// Make the API call to delete the venue
+	err := c.doRequest("DELETE", fmt.Sprintf("venue/%d", id), nil, nil)
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
 
